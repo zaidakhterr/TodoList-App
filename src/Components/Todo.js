8b5f07c603a9e995d16210ae50e6afcc432f9f3a
zaidakhterr <zaidakhter1202@gmail.com>
2019-09-19 23:08:26 +0500

    Finishing touches

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -35,17 +35,21 @@ export class Todo extends Component {
         </p>
         <div className='Todo-btn-group'>
           <button
-            className='Todo-btn tooltip'
+            className={todo.isCompleted ? 'Todo-btn' : 'Todo-btn tooltip'}
             onClick={this.handleEdit}
             disabled={todo.isCompleted}>
-            <span className='tooltip-text'>Edit</span>
+            {!todo.isCompleted && (
+              <span className='tooltip-text edit'>Edit</span>
+            )}
             <i className='fas fa-pen'></i>
           </button>{' '}
           <button
-            className='Todo-btn tooltip'
+            className={todo.isCompleted ? 'Todo-btn' : 'Todo-btn tooltip'}
             onClick={this.handleDelete}
             disabled={todo.isCompleted}>
-            <span className='tooltip-text'>Delete</span>
+            {!todo.isCompleted && (
+              <span className='tooltip-text del'>Delete</span>
+            )}
             <i className='fas fa-trash'></i>
           </button>
         </div>
